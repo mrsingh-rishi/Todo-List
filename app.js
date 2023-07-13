5575a9d1bffc9ce5abe933d25d044c0aed908f02
Rishi Singh <rs3949427@gmail.com>
2023-07-13 12:54:57 +0530

    change something dont remember

--- app.js
+++ app.js
@@ -123,9 +123,9 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 let port = process.env.PORT;
-if(port== null || port == ""){
+if(port== null || port == "" || port == undefined){
   port == 8000;
 }
-app.listen(port, function() {
-  console.log("Server started on port 3000");
+app.listen(4000, function() {
+  console.log("Server started on port 4000");
 });
